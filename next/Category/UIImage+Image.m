cd3198c1146145dc7052bb4636c16f31bf8ed753
jok886 <jok886@sina.com>
2018-08-07 17:35:33 +0800

    Add files via upload

---
+++ next/Category/UIImage+Image.m
@@ -0,0 +1,19 @@
+//
+//  UIImage+Image.m
+//  BuDeJie
+//
+//  Created by xiaomage on 16/3/11.
+//  Copyright © 2016年 小码哥. All rights reserved.
+//
+
+#import "UIImage+Image.h"
+
+@implementation UIImage (Image)
++ (UIImage *)imageOriginalWithName:(NSString *)imageName
+{
+    UIImage *image = [UIImage imageNamed:imageName];
+    
+    return [image imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+
+}
+@end
