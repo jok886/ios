662a3376b8187c5a2cf402ed7e95019386576eae
jok886 <jok886@sina.com>
2018-08-22 17:15:35 +0800

    Add files via upload

---
+++ next/换肤-4/换肤-1Tests/___1Tests.m
@@ -0,0 +1,39 @@
+//
+//  ___1Tests.m
+//  换肤-1Tests
+//
+//  Created by 王顺子 on 15/9/21.
+//  Copyright © 2015年 小码哥. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ___1Tests : XCTestCase
+
+@end
+
+@implementation ___1Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
